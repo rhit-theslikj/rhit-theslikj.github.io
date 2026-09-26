ee54e5e9977db7a9fa6056bf943c05b12fd29c37
rhit-theslikj <theslikj@rose-hulman.edu>
2026-09-25 23:36:57 -0400

    Javascript

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+document.getElementById("PedigreeCreator").addEventListener("click", function() {
+    document.getElementById("dropBoxPC").classList.toggle("dropdownReveal");
+});
+document.getElementById("PuzzlingDreams").addEventListener("click", function() {
+    document.getElementById("dropBoxPD").classList.toggle("dropdownReveal");
+});
+document.getElementById("PythonDataVisualizer").addEventListener("click", function(){
+    document.getElementById("dropBoxPDV").classList.toggle("dropdownReveal");
+});
